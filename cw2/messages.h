368dc1b3dfedf1393df2fcb95d732524b6f8d478
kisyamane <yuraserb05@gmail.com>
2024-04-20 20:53:54 +0300

    Kisya so big

---
+++ cw2/messages.h
@@ -0,0 +1,16 @@
+#define HELP "Use --help or -h for more information."
+#define MISS_ARGS "Required argument is missing for "
+#define UNKNOWN_ARG "Unknown argument or missing value for "
+#define DEPENDECIES "Required dependencies is missing for "
+#define WHAT "Something went wrong, but what?"
+#define ONLY_ONE "You're trying to run two options at the same time. Sorry, that's not how it works."
+#define WRONG_VALUE "Wrong value for "
+#define OUTPUT "Output file have the same name as input file, or output file is missing"
+#define INPUT "Input file is missing"
+#define WRONG_FILE "That's the wrong file"
+
+
+
+
+#define PRINT_HELP "WHATEVER"
+#define PRINT_INFO "WHATEVER"
